a191bfe225e597050b4d33f4bf8469ca4d793a7f
Lisanne van Vliet <lisannevanvliet@mail.com>
2024-01-21 11:57:34 +0100

    Implement single page form submit

---
+++ a2-scripting/script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form")
+form.addEventListener("submit", async event => {
+    event.preventDefault()
+
+    const formData = new FormData(form)
+    const response = await fetch("https://webtech.labs.vu.nl/api24/f81597a7", {
+        method: "POST",
+        body: new URLSearchParams(formData)
+    })
+    const result = await response.json()
+    console.log(result)
+    
+    const tr = document.createElement("tr")
+    tr.innerHTML = `
+        <td><img src="${formData.get("poster")}" alt="Poster ${formData.get("name")}"></td>
+        <td>${formData.get("name")}</td>
+        <td>${formData.get("year")}</td>
+        <td>${formData.get("genre")}</td>
+        <td>${formData.get("description")}</td>
+    `
+    document.querySelector("tbody").appendChild(tr)
+})
